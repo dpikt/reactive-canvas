5a7a24fe00155a3ab6501c9989586f3e9e61f56c
David Pickart <davidstoreypickart@gmail.com>
2017-02-17 17:02:43 -0600

    Rename line prop to thickness

--- src/shapes/Line.js
+++ src/shapes/Line.js
@@ -4,13 +4,13 @@ class Line extends Component {
   defaultProps () {
     return {
       color: 'black',
-      width: 1,
-      // endX: 100,
-      // endY: 100
+      thickness: 1,
+      endX: 100,
+      endY: 100
     }
   }
   draw (ctx) {
-    const { centerX, centerY, endX, endY, color, width, scale } = this.props
+    const { centerX, centerY, endX, endY, color, thickness, scale } = this.props
     ctx.beginPath()
     // Since end(X,Y) is relative
     const absoluteEndX = centerX + endX * scale
@@ -18,7 +18,7 @@ class Line extends Component {
 
     ctx.moveTo(centerX, centerY)
     ctx.strokeStyle = color
-    ctx.lineWidth = width
+    ctx.lineWidth = thickness
     ctx.lineTo(absoluteEndX, absoluteEndY)
     ctx.stroke()
   }
