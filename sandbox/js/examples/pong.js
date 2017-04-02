3d1f4ee57ffeac1132e2b59386271e594bb30196
David Pickart <davidstoreypickart@gmail.com>
2017-04-02 18:45:56 -0500

    Refactor

--- sandbox/js/examples/pong.js
+++ sandbox/js/examples/pong.js
@@ -3,12 +3,17 @@ const { Component, Rectangle, Group, randomColor, Oscillator } = ReactiveCanvas
 const slowOscillator = Oscillator({ start: 1, range: 0.1, speed: 0.5 })
 
 class Window extends Component {
-  render (seconds) {
-    if (!this.state.y) this.state.y = 0
+  defaultState () {
+    return { y: 0 }
+  }
+  onCanvasClick (e) {
     if (this.events.length) this.state.y = this.events[this.events.length-1].y
+  }
+  render (seconds) {
+    const { y } = this.state
     return (
       <Group>
-        <Rectangle centerX={10} centerY={this.state.y} height={200} color="black" />
+        <Rectangle centerX={10} centerY={y} height={200} color="black" />
       </Group>
       )
   }
